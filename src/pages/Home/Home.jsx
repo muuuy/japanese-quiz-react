4a2c95b68c4ad7f6f0fcd4d1f8a18721dc02b8cb
Matthew Yu <matthewyu@Matthews-MacBook-Pro-6.local>
2024-03-23 13:49:17 -0400

    Added img styled home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Navbar from '../../components/Navbar/Navbar'
+import StudyButton from '../../components/StudyButton/StudyButton';
 
-import '../../App.css';
+import styles from './Home.module.css'
+import ShrineImage from '/japanese-shrine.jpeg';
 
 function Home() {
     return (
@@ -10,7 +12,24 @@ function Home() {
             <Navbar />
             <div className='main_container'>
                 <div className='centered-div'>
-                    <h1 className='header'>Home</h1>
+                    <div className={styles.homeDesc}>
+                        <div className={styles.leftDesc}>
+                            <h1 className='header'>Home</h1>
+                            <p className={styles.description}>
+                                A quiz that tests your hiragana and katakana knowedlge.
+                                <br></br>
+                                Click <StudyButton type='HIRAGANA' /> and/or <StudyButton type='KATAKANA' /> and choose which characters you would like to study.
+                                <br></br>
+                                <br></br>
+                                Then click <StudyButton /> in order to start the quiz.
+                                <br></br>
+                                Type in each characters rōmaji equivalent.
+                                <br></br>
+                                (e.g. あ → a)
+                            </p>
+                        </div>
+                        <img src={ShrineImage} alt='Shrine Image' className={styles.shrineImage}></img>
+                    </div>
                 </div>
             </div>
         </div>
